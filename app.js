72e4754c72595c414441ce05b303a0a7094cb0e3
rizaaamore-jpg <rizaaamore@gmail.com>
2026-01-31 11:33:06 +0700

    Create app.js

---
+++ app.js
@@ -0,0 +1,7 @@
+// Inisialisasi elemen DOM
+const avatarUpload = document.getElementById('avatar-upload');
+const avatarPreviewSmall = document.getElementById('avatar-preview-small');
+const previewAvatar = document.getElementById('preview-avatar');
+const profileName = document.getElementById('profile-name');
+const previewName = document.getElementById('preview-name');
+const profileTagline = document.getElementById('profile-tag
